48efbef21569588e20525f703676c8c194046100
Sushank Ghimire <sushank.815421@bkmc.tu.edu.np>
2025-06-09 17:59:10 +0545

    Enhance chat functionality; integrate OpenAI for message replies and update sendMessage method to handle session IDs

--- store/ChatStore.ts
+++ store/ChatStore.ts
@@ -1,5 +1,6 @@
 import { create } from 'zustand';
 import { supabase } from '~/utils/supabase';
+import OpenAI from 'openai';
 
 interface ChatHistory {
   userid: string;
@@ -10,18 +11,57 @@ interface ChatHistory {
   session_id: string;
 }
 
+const openai = new OpenAI({
+  baseURL: 'https://openrouter.ai/api/v1',
+  apiKey: process.env.AI_API_KEY,
+});
+
 interface AiChatStore {
-  sendMessage: (userid: string, message: string) => Promise<void>;
-  getChatHistory: (userid: string, sessionId: string) => Promise<ChatHistory[]>;
+  sendMessage: (
+    userid: string,
+    message: string,
+    sessionId: string | null
+  ) => Promise<{ reply: string; message: string }>;
+  getChatHistory: (userid: string, sessionId: string | null) => Promise<ChatHistory[]>;
   loading: boolean;
 }
 
 const useChatStore = create<AiChatStore>((set) => ({
   loading: false,
-  sendMessage: async (userid, message) => {
+  sendMessage: async (userid, message, sessionid) => {
     set({ loading: true });
     try {
+      const { error: userError } = await supabase
+        .from('conversations')
+        .insert({ user_id: userid, role: 'user', content: message, session_id: sessionid });
+      if (userError) throw userError;
+      const response = await openai.chat.completions.create({
+        model: 'deepseek/deepseek-r1:free',
+        messages: [
+          {
+            role: 'user',
+            content: message,
+          },
+        ],
+      });
+      const reply = response.choices[0].message.content;
+      console.log(reply);
+      const { error: aiError } = await supabase.from('conversations').insert({
+        user_id: userid,
+        role: 'assistant',
+        content: reply,
+        session_id: sessionid,
+      });
+      if (!reply) throw new Error('No response from model');
+
+      if (aiError) throw aiError;
+
+      return { message, reply };
     } catch (error) {
+      if (error as Error) {
+        throw Error(`Error while getting ai response : ${error}`);
+      }
+      return { reply: '', message: '' };
     } finally {
       set({ loading: false });
     }
